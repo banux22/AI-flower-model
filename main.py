26378f6579ee9cd98ec1d6699b7fff1eca1ca14c
APeshko <m2415803@edu.misis.ru>
2025-11-27 12:24:33 +0300

    Update main.py
    change host="0.0.0.0"

--- main.py
+++ main.py
@@ -115,4 +115,4 @@ async def predict_flower(image_data: bytes) -> PredictionResponse:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
